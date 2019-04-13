7a1d9143639935a13035a8ab6f76a1f217e58b46
user-PC\user <wert7871@gmail.com>
2019-04-14 00:51:40 +0900

    test

---
+++ src/algorithm/JavaSort.java
@@ -0,0 +1,8 @@
+package algorithm;
+
+public class JavaSort {
+	public int bubblesort(int n1, int n2) {
+		return n1*n2;
+	}
+}
+
---
+++ src/algorithm_test/JavaSortTest.java
@@ -0,0 +1,14 @@
+package algorithm_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JavaSortTest {
+
+	@Test
+	void testMultiple() {
+		fail("Not yet implemented");
+	}
+
+}
